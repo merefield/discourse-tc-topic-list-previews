92b2e274c5ca38e872002f1e4e679728c6c87a88
Kelvin Tan <kelv@discourse.org>
2025-03-26 13:53:03 +0800

    DEV: refactor BufferedProperty usage in TlpThumbnailSelector

--- javascripts/discourse/components/modal/tlp-thumbnail-selector.js
+++ javascripts/discourse/components/modal/tlp-thumbnail-selector.js
@@ -1,8 +1,15 @@
 import Component from "@ember/component";
-import { action } from "@ember/object";
-import { bufferedProperty } from "discourse/mixins/buffered-content";
+import { action, computed } from "@ember/object";
+import BufferedProxy from "ember-buffered-proxy/proxy";
 
-export default Component.extend(bufferedProperty("model"), {
+export default Component.extend({
+  @computed("model")
+  buffered(model) {
+    return BufferedProxy.create({
+      content: model,
+    });
+  },
+
   @action
   selectThumbnail(image_url, image_upload_id) {
     this.set("model.buffered.user_chosen_thumbnail_url", image_url);
